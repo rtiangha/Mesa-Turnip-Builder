fccb9f4f1be6297403a51788cad8ec4301801972
Shankar Vallabhan <shankarmanu@protonmail.com>
2026-03-29 18:30:22 +0000

    update to mesa 26.0.3 + ndk 30 beta1

--- build-turnip.sh
+++ build-turnip.sh
@@ -6,13 +6,13 @@ red='\033[0;31m'
 nocolor='\033[0m'
 
 # Define Android NDK version and download URL
-ndkdir="android-ndk-r29"
+ndkdir="android-ndk-r30-beta1"
 ndkver="https://dl.google.com/android/repository/${ndkdir}-linux.zip"
 sdkver="34"
 
 # Define Mesa version and download URL
-mesadir="mesa-mesa-26.0.0"
-mesaver="https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-26.0.0/mesa-mesa-26.0.0.zip?ref_type=tags"
+mesadir="mesa-mesa-26.0.3"
+mesaver="https://gitlab.freedesktop.org/mesa/mesa/-/archive/mesa-26.0.3/mesa-mesa-26.0.3.zip?ref_type=tags"
 
 # Define working directories
 workdir="$(pwd)/turnip_workdir"         # Base directory for all operations
@@ -21,8 +21,8 @@ magiskdir="$workdir/turnip_module"      # Directory to create the Magisk module
 DRIVER_FILE="vulkan.turnip.so"          # Output Vulkan Driver (emulator)
 META_FILE="meta.json"                   # Metadata
 
-ZIP_FILE_MAGISK="Turnip-26.0.0-MAGISK-KSU.zip"
-ZIP_FILE_EMULATOR="Turnip-26-0.0-EMULATOR.zip" 
+ZIP_FILE_MAGISK="Turnip-26.0.3-MAGISK-KSU.zip"
+ZIP_FILE_EMULATOR="Turnip-26-0.3-EMULATOR.zip" 
 
 # List of required packages to build the Turnip driver
 deps="meson ninja patchelf unzip curl pip flex bison zip glslang"
@@ -233,8 +233,8 @@ EOF
 cat <<EOF >"module.prop"
 id=turnip-mesa
 name=Freedreno Turnip Vulkan Driver STABLE
-version=v26.0.0
-versionCode=20260212
+version=v26.0.3
+versionCode=20260329
 author=V3KT0R-87
 description=Turnip is an open-source vulkan driver for devices with Adreno 6xx-7xx GPUs.
 updateJson=https://raw.githubusercontent.com/v3kt0r-87/Mesa-Turnip-Builder/refs/heads/stable/update.json
